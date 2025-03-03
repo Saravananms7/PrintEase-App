0b431126c3e434cecc895ed3f1bb0b7b7ffc5de4
Saravanan <msx3gamer1@gmail.com>
2025-03-04 00:34:38 +0530

    Implement authentication-based navigation in App widget

--- lib/app.dart
+++ lib/app.dart
@@ -1,19 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:t_store/features/authentication/screens/onboarding/onbording.dart';
 import 'package:t_store/utils/theme/theme.dart';
+import 'package:t_store/navigation_menu.dart';
 
-
-class App extends StatelessWidget{
+class App extends StatelessWidget {
   const App({super.key});
 
   @override
-    Widget build(BuildContext context) {
-      return GetMaterialApp(
-        themeMode: ThemeMode.system,
-        theme: TAppTheme.lightTheme,
-        darkTheme: TAppTheme.darkTheme,
-        home:const OnBoardingScreen(),
-      );
-    }
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      themeMode: ThemeMode.system,
+      theme: TAppTheme.lightTheme,
+      darkTheme: TAppTheme.darkTheme,
+      home: _checkAuthAndNavigate(),
+    );
+  }
+
+  Widget _checkAuthAndNavigate() {
+    final user = Supabase.instance.client.auth.currentUser;
+    return user != null ? const NavigationMenu() : const OnBoardingScreen();
+  }
 }
